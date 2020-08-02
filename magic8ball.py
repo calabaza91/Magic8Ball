b70cb795d56581525370d96792e86de5b2c62e47
Caleb Cox <calebcox2010@gmail.com>
2020-08-02 10:59:56 -0500

    Add files via upload

---
+++ magic8ball.py
@@ -0,0 +1,41 @@
+#This simulates a Magic 8 Ball
+#Update 1: Add printAnswer function, user input and while loop
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again' 
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so great'
+    elif answerNumber == 9:
+        return 'Very doubtful'
+
+def printAnswer():
+    r = random.randint(1,9)
+    fortune = getAnswer(r)
+    print(fortune + '\n')
+
+while True:
+    #Input is converted to a string and is lowercased
+    getFortuneTold = str(input('Would you like to get your fortune told? (Y/N)\n')).lower()
+
+    #check if first character matches 'y' or 'n', else shake again
+    if getFortuneTold[0] == 'y':
+        printAnswer()
+    elif getFortuneTold[0] == 'n':
+        print('Goodbye and good luck..')
+        break;
+    else:
+        print('That is not a vaild answer, my dear.')
